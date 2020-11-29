edd540f1563b20b79531f02f013170338bc7d611
Surendramd <36917660+Surendramd@users.noreply.github.com>
2020-11-29 09:05:12 +0530

    Add files via upload
    FirstSession of Python

---
+++ FirstSessionPython.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+first_Name = input("What is your first name")
+Last_Name = input("What is your last name")
+print("My First name is {A} and my Last name is {B}" .format(A=first_Name, B=Last_Name))
+
+
+# In[6]:
+
+
+lst = [1, 2, 3, "SMD" , "MD"]
+
+
+# In[8]:
+
+
+lst
+
+
+# In[9]:
+
+
+lst.append("Dayannavar")
+
+
+# In[10]:
+
+
+lst
+
+
+# In[11]:
+
+
+lst[2]= "Data Scienc"
+
+
+# In[12]:
+
+
+lst
+
+
+# In[13]:
+
+
+lst.count("Dayannavar")
+
+
+# In[ ]:
+
+
+
+
